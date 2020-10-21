7e24312b7861a3828a6f2bf217fbdc4e31e7791a
thanit456 <too_w_o@hotmail.com>
2020-10-21 22:50:57 +0700

    fix config

--- modeling/layer/cosine_loss.py
+++ modeling/layer/cosine_loss.py
@@ -24,7 +24,7 @@ class ArcFace(nn.Module):
         if label is None:
             return logits 
         # add margin 
-        theta = torch.acoss(torch.clamp(logits, -1.0 + 1e-7, 1.0 - 1e-7))
+        theta = torch.acos(torch.clamp(logits, -1.0 + 1e-7, 1.0 - 1e-7))
         target_logits = torch.cos(theta + self.m)
         one_hot = torch.zeros_like(logits)
         one_hot.scatter_(1, label.view(-1, 1).long(), 1)
@@ -79,9 +79,6 @@ class AdaCos(nn.Module):
         # normalize weights 
         W = F.normalize(self.W)
         # dot product
-        print('debug logging')
-        print(x.shape)
-        print(W.shape)
         logits = F.linear(x, W)        
         if label is None:
             return logits
@@ -124,7 +121,7 @@ class CosFace(nn.Module):
         # output = logits - (1 - one_hot) + target_logits * one_hot
 
         one_hot = torch.zeros_like(logits)
-        one_hot.scatter_(1, abel.view(-1, 1).long(), 1)
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
         output = logits - one_hot * self.m
         # feature re-scale
         output *= self.s
